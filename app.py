85b58d467c62b4a16556a7404e5515dfd22b093c
TimothyRT <timothyrtzion@gmail.com>
2025-05-16 16:52:39 +0700

    testing

--- app.py
+++ app.py
@@ -85,9 +85,9 @@ def create_task():
     }, 201
 
 # Error Handlers
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
