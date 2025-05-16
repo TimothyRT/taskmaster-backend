59d7a06deeef70c5f573aa120c45182b767bf7fe
TimothyRT <timothyrtzion@gmail.com>
2025-05-16 18:09:07 +0700

    fuck

--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,18 @@
-from app import app as application
+from app import app
 from dotenv import load_dotenv
+from extensions import db  # Now imports from extensions.py
 import os
-
-from extensions import db
 
 load_dotenv()
 
-application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{username}:{password}@{host}/{db_name}'.format(
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{username}:{password}@{host}/{db_name}'.format(
     username=os.getenv('MYSQL_USER'),
     password=os.getenv('MYSQL_PASSWORD'),
     host=os.getenv('MYSQL_HOST'),
     db_name=os.getenv('MYSQL_DB')
 )
-application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-application.config['SECRET_KEY'] = os.urandom(24)
-db.init_app(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.urandom(24)
+
+db.init_app(app)  # Important: bind db to the app
 application = app
